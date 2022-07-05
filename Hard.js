f24bda14567e0520fa2875a49348f0fa964b041a
Alex Garcia <alexgarcia@Alexs-MBP.attlocal.net>
2022-07-05 17:16:34 -0400

    Hard is completed

---
+++ Hard.js
@@ -0,0 +1,17 @@
+function add(x , y){
+    console.log(x+y);
+}
+function subtract(x, y){
+    console.log(y-x);
+}
+function multiply(x,y){
+    console.log(y*x);
+}
+function divide(x,y){
+    console.log(y/x);
+}
+let a = add(2,6);
+let b = subtract(2,6);
+let c = multiply(2,6);
+let d = divide(2,6);
+
