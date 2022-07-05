c02b0245a63206b078486aa534c7c3325c880ad9
Alex Garcia <alexgarcia@Alexs-MBP.attlocal.net>
2022-07-05 19:46:44 -0400

    Very Hard and Medium

---
+++ Medium-VHard.js
@@ -0,0 +1,25 @@
+//MEDIUM
+var sentence = window.prompt("Please say something:");
+
+    if(sentence === sentence.toUpperCase()){
+        console.log("Why are you yelling..")
+    }else if(sentence === sentence.toLowerCase()){
+        console.log("Please speak up, dont whisper.")
+    }else{
+        console.log("Thank you for talking normally.") 
+    }
+
+//VERY HARD
+var num1 = window.prompt("Enter a number:");
+var operation = window.prompt("Enter an operation:");
+var num2 = window.prompt("Enter a second number:");
+
+if(operation === "+"){
+    console.log(num1+num2);
+}else if (operation === "-"){
+    console.log("num1-num2");
+}else if (operation === "*"){
+    console.log(num1 *num2);
+}else{
+    console.log(num1/num2);
+}
--- Medium.js
+++
@@ -1,9 +0,0 @@
-var sentence = window.prompt("Please say something:");
-
-    if(sentence === sentence.toUpperCase()){
-        console.log("Why are you yelling..")
-    }else if(sentence === sentence.toLowerCase()){
-        console.log("Please speak up, dont whisper.")
-    }else{
-        console.log("Thank you for talking normally.") 
-    }
